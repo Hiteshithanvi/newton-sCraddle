cf2cdebeeb3b28c4f8d2e94d0196ed0dd55e4923
Hiteshithanvi <71558813+Hiteshithanvi@users.noreply.github.com>
2021-01-31 20:37:06 +0530

    Add files via upload

---
+++ Sling.js
@@ -0,0 +1,27 @@
+class Sling{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 1,
+            length: 200
+        }
+        this.pointB = pointB;
+        this.pointx = bodyA.x;
+        this.pointy = bodyA.y-200;
+        this.sling = Constraint.create(options);
+
+        World.add(world, this.sling);
+    }
+
+    display(){
+        if(this.sling.bodyA){
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB;
+        push();
+        strokeWeight(3);
+        line(pointB.x, pointB.y, pointA.x, pointA.y);
+        pop();
+        }
+    }
+}
